88598d4ffd126612a81bcf67b45e857015bdac3a
Jaqueline Brandão <pbjackpotter@gmail.com>
2021-12-19 21:48:37 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1639960981,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/chao-sheet0.png",
+		"images/chavedefendabaixo-sheet0.png",
+		"images/chavedefenda2cima-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/bird-sheet1.png",
+		"images/tilfundo.png",
+		"images/gameover-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
